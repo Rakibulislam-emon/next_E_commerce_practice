79db6913e69a91345a17cd81d0ab028183c88c37
Rakibulislam-emon <rakibulislamemon60>
2024-11-26 21:26:01 +0600

    updated

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -9,60 +9,64 @@ import { ProductType } from '../../../../type';
 import Image from 'next/image';
 import { paymentImage } from '@/assets';
 
-export default async function ProductDetails({ params }: { params: { id: string } }) {
-    const { id } = params; // No need to await params
+type Props = {
+  params: { id: string };
+};
 
-    // Fetch product details using the provided id
-    const endpoint = `https://dummyjson.com/products/${id}`;
-    const product: ProductType = await GetData(endpoint);
+export default async function ProductDetails({ params }: Props) {
+  const { id } = params;
 
-    const savingPrice = (product.discountPercentage * product.price) / 100;
+  // Fetch product details using the provided id
+  const endpoint = `https://dummyjson.com/products/${id}`;
+  const product: ProductType = await GetData(endpoint);
 
-    return (
-        <Container className="grid grid-cols-1 md:grid-cols-2 gap-10">
-            {/* Product Image */}
-            <DetailProductImage images={product.images} />
+  const savingPrice = (product.discountPercentage * product.price) / 100;
 
-            {/* Product Details */}
-            <div>
-                <h2 className="text-2xl font-bold">{product.title}</h2>
-                <div className="flex justify-between">
-                    <ProductPrice product={product} />
-                    <h4 className="font-semibold">({product.rating} reviews)</h4>
-                </div>
-                <div className="mt-4 font-medium flex flex-col gap-y-4">
-                    <span className="flex items-center gap-x-2 mb-2">
-                        <FaRegEye />
-                        250+ people are viewing this right now
-                    </span>
-                    <h3 className="font-semibold">
-                        You are saving{' '}
-                        <span className="text-green-400">
-                            $ {savingPrice.toFixed(2)}
-                        </span>{' '}
-                        upon purchase
-                    </h3>
-                    <h3>{product.description}</h3>
-                    <h3>Brand: {product.brand}</h3>
-                    <h3>Category: {product.category}</h3>
-                    <span className="flex gap-x-2 items-center">
-                        Tags:{' '}
-                        {product.tags.map((tag, index) => (
-                            <span key={index}>
-                                {tag}
-                                {index < product.tags.length - 1 && ', '}
-                            </span>
-                        ))}
-                    </span>
-                    <AddToCartButton product={product} />
-                </div>
-                <div className="mt-4 bg-[#f7f7f7] flex flex-col items-center gap-y-2 rounded-lg mb-4 pt-2">
-                    <Image src={paymentImage} className="object-cover w-auto" alt="paymentImage" />
-                    <p className="font-bold mt-2">
-                        Guaranteed safe & secure checkout
-                    </p>
-                </div>
-            </div>
-        </Container>
-    );
+  return (
+    <Container className="grid grid-cols-1 md:grid-cols-2 gap-10">
+      {/* Product Image */}
+      <DetailProductImage images={product.images} />
+
+      {/* Product Details */}
+      <div>
+        <h2 className="text-2xl font-bold">{product.title}</h2>
+        <div className="flex justify-between">
+          <ProductPrice product={product} />
+          <h4 className="font-semibold">({product.rating} reviews)</h4>
+        </div>
+        <div className="mt-4 font-medium flex flex-col gap-y-4">
+          <span className="flex items-center gap-x-2 mb-2">
+            <FaRegEye />
+            250+ people are viewing this right now
+          </span>
+          <h3 className="font-semibold">
+            You are saving{' '}
+            <span className="text-green-400">
+              $ {savingPrice.toFixed(2)}
+            </span>{' '}
+            upon purchase
+          </h3>
+          <h3>{product.description}</h3>
+          <h3>Brand: {product.brand}</h3>
+          <h3>Category: {product.category}</h3>
+          <span className="flex gap-x-2 items-center">
+            Tags:{' '}
+            {product.tags.map((tag, index) => (
+              <span key={index}>
+                {tag}
+                {index < product.tags.length - 1 && ', '}
+              </span>
+            ))}
+          </span>
+          <AddToCartButton product={product} />
+        </div>
+        <div className="mt-4 bg-[#f7f7f7] flex flex-col items-center gap-y-2 rounded-lg mb-4 pt-2">
+          <Image src={paymentImage} className="object-cover w-auto" alt="paymentImage" />
+          <p className="font-bold mt-2">
+            Guaranteed safe & secure checkout
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
 }
